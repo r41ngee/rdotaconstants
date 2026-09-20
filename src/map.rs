abe789dfd61f7076c351038d2125d0d507d811d1
r41ngee <146991131+r41ngee@users.noreply.github.com>
2026-09-20 03:35:28 +0300

    feat: added keys and values methods in Map

--- src/map.rs
+++ src/map.rs
@@ -1,9 +1,13 @@
+use serde::{Deserialize, Serialize};
+
 pub type Pair = Vec<(String, Value)>;
 
+#[derive(Deserialize, Serialize)]
 pub struct Map {
     inner: Pair,
 }
 
+#[derive(Deserialize, Serialize)]
 pub enum Value {
     String(String),
     Map(Map),
@@ -16,5 +20,13 @@ impl Map {
 
     pub fn inner(&self) -> &Pair {
         &self.inner
+    }
+
+    pub fn keys(&self) -> Vec<&str> {
+        self.inner.iter().map(|(k, _)| k.as_str()).collect()
+    }
+
+    pub fn values(&self) -> Vec<&Value> {
+        self.inner.iter().map(|(_, v)| v).collect()
     }
 }
